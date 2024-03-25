0c1311ca5c1dbf6c9b3c04924852e8bba26c97e2
dohyun <thtree523@gmail.com>
2024-03-25 16:51:30 +0900

    [Add] gitignore add

---
+++ Memorize/Memorize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorize
+//
+//  Created by 김도현 on 2024/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
