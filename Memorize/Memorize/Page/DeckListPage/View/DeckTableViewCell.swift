20f65aab6922d81568499a7df71d3c3404cc474c
dohyun <thtree523@gmail.com>
2024-03-25 19:19:41 +0900

    [Feat] 카드덱 리스트 화면 UI 구현

---
+++ Memorize/Memorize/Page/DeckListPage/View/DeckTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  DeckTableViewCell.swift
+//  Memorize
+//
+//  Created by 김도현 on 2024/03/25.
+//
+
+import UIKit
+import SnapKit
+
+final class DeckTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "\(DeckTableViewCell.self)"
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "타이틀"
+        label.font = .systemFont(ofSize: AppResource.FontSize.contentTitle, weight: .regular)
+        label.textColor = AppResource.Color.textDarkColor
+        return label
+    }()
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "카드 갯수: 8"
+        label.font = .systemFont(ofSize: AppResource.FontSize.contentSubTitle, weight: .regular)
+        label.textColor = AppResource.Color.textSubColor
+        return label
+    }()
+    private let editButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "pencil"), for: .normal)
+        button.tintColor = AppResource.Color.mainColor
+        return button
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subTitleLabel)
+        contentView.addSubview(editButton)
+        titleLabel.snp.makeConstraints { make in
+            make.top.left.equalTo(contentView).inset(AppResource.Padding.medium)
+        }
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(AppResource.Padding.small)
+            make.left.bottom.equalTo(contentView).inset(AppResource.Padding.medium)
+        }
+        editButton.snp.makeConstraints { make in
+            make.top.right.equalTo(contentView).inset(AppResource.Padding.medium)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
